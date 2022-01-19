4d1f7285cd45c002f01a99e2d53e5e00b9aac26a
Chataeh <app4937@naver.com>
2022-01-19 17:04:11 +0900

    java

--- Lab04_1/src/Lab04_1.java
+++ Lab04_1/src/Lab04_1.java
@@ -3,21 +3,87 @@ class Rectangle{
 	int y1;
 	int x2;
 	int y2;
+	
+	Rectangle()
+	{
+		x1=0;
+		x2=0;
+		y1=0;
+		y2=0;
+	}
+	Rectangle(int x1,int y1,int x2,int y2)
+	{
+		this.x1=x1;
+		this.y1=y1;
+		this.x2=x2;
+		this.y2=y2;
+	}
+	
+	void set(int x1,int y1,int x2,int y2)
+	{
+		this.x1=x1;
+		this.y1=y1;
+		this.x2=x2;
+		this.y2=y2;
+	}
+	boolean check()
+	{
+		if(x1==x2||y1==y2)
+			return false;
+		else
+			return true;
+	}
+	
+	int getArea()
+	{
+		int width=0;
+		int x=0;
+		int y=0;
+		if(x1>x2)
+			x=x1-x2;
+		else 
+			x=x2-x1;
+		
+		if(y1>y2)
+			y=y1-y2;
+		else
+			y=y2-1;
+		width=(x*y);
+		
+		return width;
+	}
+	
+	void show()
+	{
+		if(check())
+			System.out.println("("+x1+","+y1+"), ("+x2+","+y1+"), ("+x2+","+y2+"), ("+x1+","+y2+")");
+		else
+			System.out.println("직사각형이 아닙니다");
+	}
+	
+	boolean equals(Rectangle r)
+	{
+		if(this.getArea()==r.getArea())
+			return true;
+		else
+			return false;
+	}
 }
 
 
 public class Lab04_1 {
-
 	public static void main(String[] args) {
-		Person p;
-		p=new Person(20);
-		p.name="test";
+		Rectangle r=new Rectangle();
+		Rectangle s=new Rectangle(1,1,2,3);
 		
+		r.show();
+		s.show();
 		
-		int i=p.age;
-		
-		String str=p.getName();
-		
-		System.out.print("name = "+str+"  age = "+i);
+		System.out.println(s.getArea());
+		r.set(2, 1, 4, 5);
+		r.show();
+		System.out.println(r.getArea());
+		if(r.equals(s))
+			System.out.println("두 사각형은 같습니다");
 	}
 }
--- Lab04_2/src/Lab04_2.java
+++ Lab04_2/src/Lab04_2.java
@@ -1,8 +1,13 @@
 class ComplexNumber
 {
-	double real;
-	double imag;
+	private double real;
+	private double imag;
 	
+	ComplexNumber()
+	{
+		real=0.0;
+		imag=0.0;
+	}
 	ComplexNumber(double real,double imag)
 	{
 		this.real=real;
@@ -61,40 +66,27 @@ public class Lab04_2 {
 
 	public static void main(String[] args) {
 		
-		 ComplexNumber[] n=new ComplexNumber[5];
+		ComplexNumber n1=new ComplexNumber();
+		ComplexNumber n2=new ComplexNumber();
 		 
-		 n[0]=new ComplexNumber(1.0,2.0);
-		 n[1]=new ComplexNumber(2.0,3.0);
-		 n[2]=new ComplexNumber(3.0,4.0);
-		 n[3]=new ComplexNumber(4.0,5.0);
-		 n[4]=new ComplexNumber(5.0,3.0);
-		//ComplexNumber n1=new ComplexNumber(5,7.2);
-		//ComplexNumber n2=new ComplexNumber(-3.1,5.7);
-		
-		 for(int i=0;i<n.length;i++)
-		 {
-			 n[i].printNumber();
-		 }
-		 
-		 
-		/*n1.setReal(5);
+		n1.setReal(5);
 		n1.setImag(7.2);
 		n2.setReal(-3.1);
-		n2.setImag(5.7);*/
+		n2.setImag(5.7);
 		
-		/*System.out.print("n1 is -> \t");
-		n[0].printNumber();
+		System.out.print("n1 is -> \t");
+		n1.printNumber();
 		
 		System.out.print("n2 is -> \t");
-		n[1].printNumber();
+		n2.printNumber();
 		
-		ComplexNumber n3=n[0].add(n[1]);
+		ComplexNumber n3=n1.add(n2);
 		
 		System.out.print("n1 + n2 is -> \t");
 		n3.printNumber();
 		
 		System.out.print("n1 - n2 is -> \t");
-		n[0].subtract(n[1]).printNumber();*/
+		n1.subtract(n2).printNumber();
 	}
 
 }
